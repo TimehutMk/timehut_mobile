ee0160d1c4c8416f4accefb6674b0892924d8b02
JordanchoEftimov <ejordancho@gmail.com>
2023-09-17 19:51:57 +0200

    logout

--- lib/screens/employee/menu_screen.dart
+++ lib/screens/employee/menu_screen.dart
@@ -1,14 +1,40 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:timehut_mobile/app_registry.dart';
 import 'package:timehut_mobile/routers/app_router.gr.dart';
 import 'package:timehut_mobile/state/user_state.dart';
 import 'package:timehut_mobile/widgets/app_base_scaffold.dart';
 import 'package:timehut_mobile/widgets/custom_button.dart';
 
 @RoutePage()
-class MenuScreen extends StatelessWidget {
-  const MenuScreen({Key? key}) : super(key: key);
+class MenuScreen extends StatefulWidget {
+  const MenuScreen({super.key});
+
+  @override
+  State<MenuScreen> createState() => _MenuScreenState();
+}
+
+class _MenuScreenState extends State<MenuScreen> {
+  final dio = AppRegistry.i.dio;
+
+  Future<void> logout() async {
+    try {
+      // @TODO: make this also invalidate the token on the backend
+      // final res = await dio.post('/logout');
+      // if (res.statusCode != 200) return;
+      // context.read<UserState>().clearUserState();
+      AutoRouter.of(context).replaceAll([const HomeRoute()]);
+    } catch (error) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'Се случи грешка!',
+          ),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,9 +89,7 @@ class MenuScreen extends StatelessWidget {
                 buttonText: "ОДЈАВИ СЕ",
                 textColor: Colors.white,
                 backgroundColor: const Color.fromRGBO(58, 204, 225, 1),
-                function: () => AutoRouter.of(context).push(
-                  ShiftsRoute(),
-                ),
+                function: logout,
               ),
             ),
           ],
--- lib/widgets/app_base_scaffold.dart
+++ lib/widgets/app_base_scaffold.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:timehut_mobile/routers/app_router.gr.dart';
-import 'package:timehut_mobile/widgets/footer.dart';
 import 'package:timehut_mobile/widgets/logo.dart';
 
 class AppBaseScaffold extends StatelessWidget {
@@ -20,7 +19,6 @@ class AppBaseScaffold extends StatelessWidget {
               onPressed: () => AutoRouter.of(context).push(const MenuRoute()),
             ),
             child,
-            const Footer(),
           ],
         ),
       ),
